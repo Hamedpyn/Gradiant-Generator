6195167bcf50028eefcd83a7e44d04dccf0e4f45
Hamed <hamedpyn@gmail.com>
2023-09-11 14:34:29 +0430

    Select DOM Elements

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+
+// Select DOM Elements
+const bodyEl = document.querySelector('body');
+const colorA = document.querySelector('#color-a');
+const colorB = document.querySelector('#color-b');
+const submit = document.querySelector('#submit');
+const copy = document.querySelector('#copy');
+const buttons = document.querySelectorAll('.buttons > button');
+const textArea = document.querySelector('#code');
